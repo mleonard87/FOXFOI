abccbf71ea2e1d9d05b7152ef00caa7b341272fb
Robert Gwynn-Jones <gwynnr@gmail.com>
2015-01-05 16:07:10 +0700

    Remove old MP app

--- mp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from mp.models import MP
-
-class MPForm(forms.ModelForm):
-    class Meta:
-        model = MP
-        fields = ['title', 'name', 'party', 'constituency', 'address', 'postcode']
