16e3ea909fc3ab20464c079183b267a13e0eb198
Robert Gwynn-Jones <gwynnr@gmail.com>
2015-01-06 17:19:18 +0700

    Remove 'parent' field from Key Term form

--- keyterms/forms.py
+++ keyterms/forms.py
@@ -4,7 +4,7 @@ from keyterms.models import KeyTerm
 class KeyTermForm(forms.ModelForm):
     class Meta:
         model = KeyTerm
-        fields = ['name', 'parent']
+        fields = ['name']
         widgets = {
             'name': forms.TextInput(attrs = {'placeholder': 'Enter a name for this Key Term'})
         }
