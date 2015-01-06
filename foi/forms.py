151529c170bd55c51c7f29f0f2eead20fedfef9b
Robert Gwynn-Jones <gwynnr@gmail.com>
2015-01-06 22:41:12 +0700

    Update all date fields to use date pickers, and change label of Case Detail 'urgent flag' to 'Urgent?'

--- foi/forms.py
+++ foi/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from foi.models import Case, Comment, InternalReview, InformationCommissionerAppeal, AdministrativeAppealsTribunal
 
 class CaseForm(forms.ModelForm):
@@ -16,8 +17,14 @@ class CaseForm(forms.ModelForm):
             'urgent_flag', 
             'handling_instructions',
             'addressee_name']
+        labels = {
+            'urgent_flag': ('Urgent?')
+        }
         widgets = {
-            'title': forms.TextInput(attrs = {'placeholder': 'Enter a title for this case'})
+            'title': forms.TextInput(attrs = {'placeholder': 'Enter a title for this case'}),
+            'received_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'enquiry_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'urgent_flag': forms.CheckboxInput(attrs = {'label': 'Urgent?'})
         }
 
 class CommentForm(forms.ModelForm):
@@ -33,13 +40,27 @@ class InternalReviewForm(forms.ModelForm):
     class Meta:
         model = InternalReview
         fields = ['requested_date', 'review_held_date', 'days_taken_to_hold_review', 'review_members', 'review_decision']
+        widgets = {
+            'requested_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'review_held_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()})
+        }
 
 class InformationCommissionerAppealForm(forms.ModelForm):
     class Meta:
         model = InformationCommissionerAppeal
         fields = ['contacted_date', 'documents_provided_date', 'decision_recieved_date', 'decision', 'decision_notice']
+        widgets = {
+            'contacted_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'documents_provided_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'decision_recieved_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()})
+        }
 
 class AdministrativeAppealsTribunalForm(forms.ModelForm):
     class Meta:
         model = AdministrativeAppealsTribunal
         fields = ['contacted_date', 'documents_provided_date', 'decision_recieved_date', 'decision', 'decision_notice']
+        widgets = {
+            'contacted_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'documents_provided_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()}),
+            'decision_recieved_date': forms.DateInput(attrs = {'type': 'date', 'value': timezone.now().date()})
+        }
